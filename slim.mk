3d8491d7ae0f6eea2ae38ea4722aebabc140ab8a
Lars Greiss <kufikugel@googlemail.com>
2014-04-07 00:52:23 +0200

    use multicarrier overlays
    Change-Id: Ifeb6cd187e68534e3775ce3e14fb5ee54926f3a0

--- slim.mk
+++ slim.mk
@@ -9,5 +9,8 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
+# Inherit multicarrier.
+$(call inherit-product, vendor/slim/config/common_multicarrier.mk)
+
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := slim_hlte
