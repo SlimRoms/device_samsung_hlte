60e159d0b9e1f5ec0c042d0e3b73c802686d80f4
cordell <cordellsmail@gmail.com>
2014-04-11 12:08:26 +0200

    remove gsm.mk
    Change-Id: Ib26751e2649a08c5233da13be17f2d90d4e7106d

--- slim.mk
+++ slim.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
